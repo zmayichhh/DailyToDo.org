4a9922a30f9499da1465a1f965f9085d1005604b
zmayichhh <smajic96@hotmail.com>
2023-11-02 11:29:34 +0100

    updated first test

--- Automation/cypress.config.js
+++ Automation/cypress.config.js
@@ -1,7 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  defaultCommandTimeout: 6000,
+  env: {
+    url: "https://dailytodo.org",
+  },
   e2e: {
+    specPattern: "cypress/integration/examples/spec.js",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
